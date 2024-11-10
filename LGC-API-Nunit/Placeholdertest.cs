70deaa66306b6bdb1b09f33b93cb808716308a50
AndrewSkelly <andrewskelly@hotmail.co.uk>
2024-11-10 18:06:06 +0000

    Final Update

--- LGC-API-Nunit/Placeholdertest.cs
+++ LGC-API-Nunit/Placeholdertest.cs
@@ -10,6 +10,7 @@ using RestSharp;
 
 // Used this for refference https://dev.to/m4rri4nne/nunit-and-c-tutorial-to-automate-your-api-tests-from-scratch-24nf
 // Refference for using RestSharp https://restsharp.dev/docs/usage/request
+// Convert raw JSON to string https://dadroit.com/json-to-string/
 
 namespace LGC_API_Nunit
 {
@@ -53,8 +54,8 @@ namespace LGC_API_Nunit
             // Here the response is handled
             RestResponse restResponse = client.Execute(restRequest);
 
-            // Assert that the body contains the body wwe sent
-            restResponse.Content.Should().Contain(body.ToString());
+            // Assert that the body contains the body we sent
+            restResponse.Content.Should().Contain("{\n  \"id\": 101,\n  \"title\": \"New Car\",\n  \"body\": \"Today I bought a new car!\"\n}");
 
             // Should also be a 201 status of Created
             restResponse.StatusCode.Should().Be(HttpStatusCode.Created);
@@ -66,12 +67,18 @@ namespace LGC_API_Nunit
             var baseUrl = "https://jsonplaceholder.typicode.com/posts/1";
             RestClient client = new RestClient(baseUrl);
 
+            // Create new Object from my model
+            var body = new PostsModel { Title = "Update on car", Body = "The car broke down!" };
+
+
             // Create a request to the PlaceHolder API using Get method
-            RestRequest restRequest = new RestRequest(baseUrl, Method.Get);
+            RestRequest restRequest = new RestRequest(baseUrl, Method.Put);
+            restRequest.AddBody(body, ContentType.Json); // I tagged on the body object
             // Here the response is handled
             RestResponse restResponse = client.Execute(restRequest);
 
-            Console.WriteLine(restResponse);
+            // Assert that the body contains the body we sent
+            restResponse.Content.Should().Contain("{\n  \"id\": 1,\n  \"title\": \"Update on car\",\n  \"body\": \"The car broke down!\"\n}");
 
             // Assert that the response will not be empty
             restResponse.Should().NotBeNull();
@@ -87,17 +94,16 @@ namespace LGC_API_Nunit
             RestClient client = new RestClient(baseUrl);
 
             // Create a request to the PlaceHolder API using Get method
-            RestRequest restRequest = new RestRequest(baseUrl, Method.Get);
+            RestRequest restRequest = new RestRequest(baseUrl, Method.Delete);
             // Here the response is handled
             RestResponse restResponse = client.Execute(restRequest);
 
             Console.WriteLine(restResponse);
 
-            // Assert that the response will not be empty
-            restResponse.Should().NotBeNull();
-            // restResponse.Should().Be();
+            // Assert that the response is Null or Empty
+            restResponse.Content.Should().Be("{}");
             // Should also be a 200 status of OK
-            restResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            restResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
         }
     }
